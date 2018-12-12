3c928c9368a16bcfb35c5f76b8c66a3dc31d41f9
Allan Tricarico <allan.ts@outlook.com>
2018-12-12 19:18:22 -0200

    Exercício 2.6

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,19 +2,41 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
-	
+
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private LocalTime horario;
+
+	@ManyToOne
 	private Sala sala;
+
+	@ManyToOne
 	private Filme filme;
-	
-	public Sessao(LocalTime horario, Filme filme, Sala sala) {
-		this.horario = horario;
-		this.filme = filme;
-		this.sala = sala;
+
+	/**
+	 * @param sala2
+	 * @param filme2
+	 * @param horario2
+	 * @deprecated hibernate only
+	 */
+	public Sessao() {
+
 	}
-	
+
+	public Sessao(LocalTime horario, Sala sala, Filme filme) {
+		this.horario = horario;
+		this.sala = sala;
+		this.filme = filme;
+	}
+
 	public LocalTime getHorarioTermino() {
 		return this.horario.plusMinutes(filme.getDuracao().toMinutes());
 	}
@@ -50,7 +72,5 @@ public class Sessao {
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
-	
-	
 
 }
